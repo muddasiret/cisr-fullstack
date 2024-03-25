165675f02e5bbb1a912394ead44bfff7537ba1f5
muddasir <muddasir.e@itilite.com>
2024-03-25 17:59:32 +0530

    baseurl removed

--- src/app/utils/axiosInterceptor.jsx
+++ src/app/utils/axiosInterceptor.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL, // Set your base URL here
-});
+const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(
   (config) => {
