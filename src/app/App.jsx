68a49d1bdea21934b2f5b1321c9220c232ec0eda
muddasir <muddasir.e@itilite.com>
2024-03-25 20:31:31 +0530

    login page added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,24 +1,44 @@
-import React, { Suspense, lazy } from "react";
+// App.js
+import React, { Suspense, lazy, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
+import Login from "./pages/login/Login";
 
 const News = lazy(() => import("./pages/news/News"));
 const Events = lazy(() => import("./pages/events/Events"));
 const NotFound = () => <div>Not Found</div>; // Define a NotFound component
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+
+  useEffect(() => {
+    if (!isLoggedIn && window.location.pathname !== "/login") {
+      window.location.href = "/login";
+    }
+  }, []);
+
   return (
     <Router>
-      <AppLayout>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={News} />
-            <Route path="/news" component={News} />
-            <Route path="/events" component={Events} />
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </AppLayout>
+      <Switch>
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <AppLayout>
+          <Suspense fallback={<div>Loading...</div>}>
+            {isLoggedIn && (
+              <>
+                <Route exact path="/" component={News} />
+                <Route path="/news" component={News} />
+                <Route path="/events" component={Events} />
+              </>
+            )}
+          </Suspense>
+          {/* <Route component={NotFound} /> */}
+        </AppLayout>
+      </Switch>
     </Router>
   );
 }
---
+++ src/app/pages/login/Login.jsx
@@ -0,0 +1,64 @@
+// Login.js
+import { Button, Form, Input, Typography } from "antd";
+import React from "react";
+import { useHistory } from "react-router-dom";
+const { Title } = Typography;
+
+const Login = ({ setIsLoggedIn }) => {
+  const [form] = Form.useForm();
+  const history = useHistory();
+  const handleLogin = ({ email, password }) => {
+    // Perform validation and authentication logic here
+    // For simplicity, let's assume email and password are correct
+    console.log(email, password);
+    const isLoggedIn = email === "user@example.com" && password === "password";
+    if (isLoggedIn) {
+      localStorage.setItem("isLoggedIn", "true");
+      history.push("/");
+      setIsLoggedIn(true);
+    } else {
+      alert("Invalid email or password");
+    }
+  };
+
+  return (
+    <div>
+      <div
+        className="p-2 flex mb-6 flex-col justify-start items-start bg-blue-950"
+        // style={{ borderBottom: "0.5px solid grey" }}
+      >
+        <img src="/cisr_logo.png" style={{ height: 40 }} />
+        <div className=" italic text-white mt-3">Admin dashboard</div>
+      </div>
+      <div className="m-4">
+        <Title className="m-0" level={3}>
+          Login
+        </Title>
+        <Form
+          className="form-popup"
+          form={form}
+          labelCol={{
+            span: 4,
+          }}
+          layout="vertical"
+          wrapperCol={{
+            span: 20,
+          }}
+          onFinish={() => {
+            handleLogin(form.getFieldsValue());
+          }}
+        >
+          <Form.Item label="Email" name={"email"}>
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" />
+          </Form.Item>
+          <Button htmlType="submit">Login</Button>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
